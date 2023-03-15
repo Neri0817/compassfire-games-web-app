86d751f6c097dd6ad80fea751b3dc6f23a61a7da
Neri <91215462+Neri0817@users.noreply.github.com>
2023-03-15 18:24:46 +0200

    Add about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,7 +1,61 @@
 export const About = () => {
   return (
     <section className="about-section">
-      
+      <h1 className="about-section-heading">About Us</h1>
+      <div className="about-section-header">
+        <div className="about-section-header-content">
+          <span>Compass</span>
+          <h1>
+            All games. Every platform. <br /> One place.
+          </h1>
+        </div>
+        <div className="about-section-header-img"></div>
+      </div>
+
+      <div className="about-section-main">
+        <p className="about-section-main-p">
+          Lorem ipsum dolor sit amet, est in dico elit scriptorem. Eam te eirmod
+          suscipit appareat, per possit facilis ea, cu nec dictas constituto.
+          Modus epicurei no qui, pri dignissim sententiae an. Accusam suavitate
+          his eu, duis doctus eum cu. No summo conceptam per, ignota salutatus
+          conceptam eu mei. In pri novum saepe eirmod.
+        </p>
+
+        <div className="about-section-main-items">
+          <div className="about-section-main-items-info">
+            <i className="fa-solid fa-gamepad"></i>
+            <h3>Lorem ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, est in dico elit scriptorem. Eam te
+              eirmod suscipit appareat.
+            </p>
+          </div>
+          <div className="about-section-main-items-info">
+            <i className="fa-solid fa-heart"></i>
+            <h3>Lorem ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, est in dico elit scriptorem. Eam te
+              eirmod suscipit appareat.
+            </p>
+          </div>
+          <div className="about-section-main-items-info">
+            <i class="fa-solid fa-fire-flame-curved"></i>
+            <h3>Lorem ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, est in dico elit scriptorem. Eam te
+              eirmod suscipit appareat.
+            </p>
+          </div>
+          <div className="about-section-main-items-info">
+            <i class="fa-solid fa-trophy"></i>
+            <h3>Lorem ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet, est in dico elit scriptorem. Eam te
+              eirmod suscipit appareat.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
