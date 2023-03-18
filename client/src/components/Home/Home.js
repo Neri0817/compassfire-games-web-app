8535ce601d8a9b0727be298e802cd65000b7869b
Neri <91215462+Neri0817@users.noreply.github.com>
2023-03-18 20:08:59 +0200

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
   return (
     <section className="home-section">
@@ -6,7 +8,7 @@ export const Home = () => {
           Discover games <br /> that you enjoy
         </h1>
         <h4>Read about your favourite games</h4>
-        <a href="/gameshelf">Start Exploaring</a>
+        <Link to="/gameshelf">Start Exploaring</Link>
       </div>
       <div className="home-section-img"></div>
     </section>
