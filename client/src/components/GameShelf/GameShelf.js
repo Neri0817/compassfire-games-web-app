284be1ee3ea72ac400b6c12f584576d542b8367b
Neri <91215462+Neri0817@users.noreply.github.com>
2023-03-21 19:18:53 +0200

    Update

--- client/src/components/GameShelf/GameShelf.js
+++ client/src/components/GameShelf/GameShelf.js
@@ -24,16 +24,17 @@ export const GameShelf = ({ games }) => {
             type="search"
             placeholder="Search your game..."
           />
-          {/* onChange={} value={} */}
           <button type="submit" className="gameshelf-section-search-btn">
             <i className="fa-solid fa-magnifying-glass"></i>
           </button>
         </form>
       </div>
       <div className="gameshelf-section-games">
-        {/* {games.map((game) => (
-          <Game key={game._id} {...game} />
-        ))} */}
+        {games.length === 0 && (
+          <div className="gameshelf-section-games-empty">
+            <h2>No games added yet...</h2>
+          </div>
+        )}
 
         {filteredData.map((game) => (
           <Game key={game._id} {...game} />
