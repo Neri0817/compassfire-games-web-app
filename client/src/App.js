88fd3763f510ab7efa8a5c0b20f3ba7566d6a5c4
Neri <91215462+Neri0817@users.noreply.github.com>
2023-03-25 14:53:22 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,32 +8,74 @@ import { AddGame } from "./components/AddGame/AddGame";
 import { GameDetails } from "./components/GameDetails/GameDetails";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
-import { Logout } from "./components/Logout/Logout";
-import { EditGame } from "./components/EditGame/EditGame";
 
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import { gameServiceFactory } from "./services/gameService";
-import { AuthProvider } from "./contexts/AuthContext";
+import { authServiceFactory } from "./services/authService";
+import { AuthContext } from "./contexts/AuthContext";
+import { Logout } from "./components/Logout/Logout";
+
+const baseUrl = "http://localhost:3030/data/games";
 
 function App() {
   const navigate = useNavigate();
   const [games, setGames] = useState([]);
-  const gameService = gameServiceFactory(); //auth.accessToken
+  const [auth, setAuth] = useState({});
+
+  const gameService = gameServiceFactory(auth.accessToken);
+  const authService = authServiceFactory(auth.accessToken);
 
   useEffect(() => {
-    gameService.getAll().then((result) => {
-      setGames(result);
-    });
-  }, [gameService]);
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setGames(Object.values(data));
+      });
+  }, []);
 
   const onAddGameSubmit = async (data) => {
-    const newGame = await gameService.create(data);
+    const addedGame = await gameService.addGame(data);
 
-    setGames((state) => [...state, newGame]);
+    setGames((games) => [...games, addedGame]);
 
     navigate("/gameshelf");
+  };
+
+  const onLoginSubmit = async (data) => {
+    try {
+      const result = await authService.login(data);
+
+      setAuth(result);
+
+      navigate("/");
+    } catch (error) {
+      console.log("There is a problem");
+    }
+  };
+
+  const onRegisterSubmit = async (values) => {
+    const { confirmPassword, ...registerData } = values;
+    if (confirmPassword !== registerData.password) {
+      return;
+    }
+
+    try {
+      const result = await authService.register(registerData);
+
+      setAuth(result);
+
+      navigate("/gameshelf");
+    } catch (error) {
+      console.log("There is a problem");
+    }
+  };
+
+  const onLogout = async () => {
+    await authService.logout();
+
+    setAuth({});
   };
 
   const onGameEditSubmit = async (values) => {
@@ -41,11 +83,21 @@ function App() {
 
     setGames((state) => state.map((x) => (x._id === values._id ? result : x)));
 
-    navigate(`/gameshelf/${values._id}`);
+    navigate(`/catalog/${values._id}`);
+  };
+
+  const contextValues = {
+    onLoginSubmit,
+    onRegisterSubmit,
+    onLogout,
+    userId: auth._id,
+    token: auth.accessToken,
+    userEmail: auth.email,
+    isAuthenticated: !!auth.accessToken,
   };
 
   return (
-    <AuthProvider>
+    <AuthContext.Provider value={contextValues}>
       <Header />
 
       <main>
@@ -53,14 +105,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/gameshelf" element={<GameShelf games={games} />} />
-          <Route path="/gameshelf/:gameId" element={<GameDetails />} />
+          <Route path="/gameshelf/:gameId" element={<GameDetails onGameEditSubmit={onGameEditSubmit}/>} />
           <Route
             path="/addgame"
             element={<AddGame onAddGameSubmit={onAddGameSubmit} />}
-          />
-          <Route
-            path="/gameshelf/:gameId/edit"
-            element={<EditGame onGameEditSubmit={onGameEditSubmit} />}
           />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
@@ -69,8 +117,7 @@ function App() {
       </main>
 
       <Footer />
-    </AuthProvider>
+    </AuthContext.Provider>
   );
 }
-
 export default App;
