1e60ec1a801758408690fe4af6fba0c2b9e1d8f8
Neri <91215462+Neri0817@users.noreply.github.com>
2023-04-06 21:40:55 +0300

    Update add game validation

--- client/src/components/AddGame/AddGame.js
+++ client/src/components/AddGame/AddGame.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import { useGameContext } from "../../contexts/GameContext";
 import { useForm } from "../../hooks/useForm";
+import { validation } from "../../services/validation";
 
 export const AddGame = () => {
   const { onAddGameSubmit } = useGameContext();
@@ -13,6 +15,22 @@ export const AddGame = () => {
     onAddGameSubmit
   );
 
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    setErrors(validation(values));
+  }, [values]);
+
+  const handleBlur = (event) => {
+    const { name, value } = event.target;
+
+    // Validate the value
+    const error = validation(name, value);
+
+    // Update the errors state
+    setErrors({ ...errors, [name]: error });
+  };
+
   return (
     <section className="addgame-section">
       <h1 className="addgame-section-heading">Add Game</h1>
@@ -22,46 +40,79 @@ export const AddGame = () => {
           <form onSubmit={onSubmit}>
             <div>
               <label>Name:</label>
+              {errors.text && (
+                <p className="errors-style">
+                  <i className="fa-solid fa-triangle-exclamation fa-beat-fade"></i>
+                  &nbsp; &nbsp;
+                  {errors.text}
+                </p>
+              )}
               <input
                 type="text"
                 id="title"
                 name="title"
                 value={values.title}
                 onChange={changeHandler}
+                onBlur={handleBlur}
                 placeholder="Game Name"
               />
             </div>
             <div>
               <label>Genre:</label>
+              {errors.text && (
+                <p className="errors-style">
+                  <i className="fa-solid fa-triangle-exclamation fa-beat-fade"></i>
+                  &nbsp; &nbsp;
+                  {errors.text}
+                </p>
+              )}
               <input
                 type="text"
                 id="category"
                 name="category"
                 value={values.category}
                 onChange={changeHandler}
+                onBlur={handleBlur}
                 placeholder="Game Genre"
               />
             </div>
             <div>
               <label>Image:</label>
+              {errors.text && (
+                <p className="errors-style">
+                  <i className="fa-solid fa-triangle-exclamation fa-beat-fade"></i>
+                  &nbsp; &nbsp;
+                  {errors.text}
+                </p>
+              )}
               <input
                 type="text"
                 id="imageUrl"
                 name="imageUrl"
                 value={values.imageUrl}
                 onChange={changeHandler}
+                onBlur={handleBlur}
                 placeholder="Upload an image..."
               />
             </div>
             <div>
               <label>Description:</label>
+              {errors.text && (
+                <p className="errors-style">
+                  <i className="fa-solid fa-triangle-exclamation fa-beat-fade"></i>
+                  &nbsp; &nbsp;
+                  {errors.text}
+                </p>
+              )}
               <textarea
+                type="text"
                 id="summary"
                 cols="50"
                 rows="5"
                 name="summary"
                 value={values.summary}
                 onChange={changeHandler}
+                onBlur={handleBlur}
                 placeholder="Game Description..."
               ></textarea>
             </div>
